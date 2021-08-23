31da42ab49c6fec4724b1e952afa24a82e76c6d1
Diego <diegomcarvalho7@gmail.com>
2021-08-23 10:47:53 -0300

    Código do projeto

---
+++ Alura.LeilaoOnline.Core/Interessada.cs
@@ -0,0 +1,14 @@
+﻿namespace Alura.LeilaoOnline.Core
+{
+    public class Interessada
+    {
+        public string Nome { get; }
+        public Leilao Leilao { get; }
+
+        public Interessada(string nome, Leilao leilao)
+        {
+            this.Nome = nome;
+            this.Leilao = leilao;
+        }
+    }
+}
