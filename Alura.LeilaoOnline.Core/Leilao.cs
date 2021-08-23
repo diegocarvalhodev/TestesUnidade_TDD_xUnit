909e51f6414e078d0b7050a445ca688d9b7649a2
Diego <diegomcarvalho7@gmail.com>
2021-08-23 13:53:07 -0300

    Diferença do teste de Fato (Fact) e Teoria (Theory)

--- Alura.LeilaoOnline.Core/Leilao.cs
+++ Alura.LeilaoOnline.Core/Leilao.cs
@@ -3,20 +3,33 @@ using System.Linq;
 
 namespace Alura.LeilaoOnline.Core
 {
+    public enum EstadoLeilao
+    {
+        LeilaoEmAndamento,
+        LeilaoFinalizado
+    }
+    
+    
     public class Leilao
     {
         private IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; }
         public Lance Ganhador { get; private set; }
+        public EstadoLeilao Estado { get; private set; }
 
         public Leilao(string peca)
         {
             this.Peca = peca;
             this._lances = new List<Lance>();
+            this.Estado = EstadoLeilao.LeilaoEmAndamento;
         }
 
-        public void RecebeLance(Interessada cliente, double valor) => this._lances.Add(new Lance(cliente, valor));
+        public void RecebeLance(Interessada cliente, double valor)
+        {
+            if (this.Estado == EstadoLeilao.LeilaoEmAndamento)
+                this._lances.Add(new Lance(cliente, valor));
+        }
 
         public void IniciaPregao()
         {
@@ -24,9 +37,13 @@ namespace Alura.LeilaoOnline.Core
         }
 
         public void TerminaPregao()
-            => this.Ganhador = this.Lances
-                                        .DefaultIfEmpty(new Lance(null, 0))
-                                        .OrderBy(l => l.Valor)
-                                        .LastOrDefault();
+        {
+            this.Ganhador = this.Lances
+                                    .DefaultIfEmpty(new Lance(null, 0))
+                                    .OrderBy(l => l.Valor)
+                                    .LastOrDefault();
+
+            this.Estado = EstadoLeilao.LeilaoFinalizado;
+        }
     }
 }
---
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeLance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Alura.LeilaoOnline.Core;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeLance
+    {
+        [Theory]
+        [InlineData(2, new double[] { 800, 900 })]
+        [InlineData(4, new double[] { 1000, 1200, 1400, 1300})]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(int qtdEsperada, double[] ofertas)
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+            
+            leilao.TerminaPregao();
+
+            //Act - método sob teste
+            //Testar se permite adicionar lanche após término do leilão
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            var qtdObtida = leilao.Lances.Count();
+            Assert.Equal(qtdEsperada, qtdObtida);
+        }
+
+    }
+}
